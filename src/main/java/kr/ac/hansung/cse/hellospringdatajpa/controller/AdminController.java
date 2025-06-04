339b588097619dfa67fe016a99a5c0cbd0dd0153
oh-chaeyeon <chaebread555@gmail.com>
2025-06-04 19:24:39 +0900

    Feat : 관리자만 접속가능한 유저목록페이지 생성

---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/controller/AdminController.java
@@ -0,0 +1,28 @@
+package kr.ac.hansung.cse.hellospringdatajpa.controller;
+
+import kr.ac.hansung.cse.hellospringdatajpa.entity.User;
+import kr.ac.hansung.cse.hellospringdatajpa.repo.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    private final UserRepository userRepository;
+
+    public AdminController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin/users")
+    public String showUserList(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "user_list";
+    }
+}
